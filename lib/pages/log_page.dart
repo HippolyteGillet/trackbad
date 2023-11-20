1093199d66601b9baf9222e6ca8be8ac7f4abe75
HippolyteGillet <hippolyte.gillet@mobifactory.fr>
2023-11-20 17:05:52 +0100

    first pages and navbar

---
+++ lib/pages/log_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LogPage extends StatefulWidget {
+  const LogPage({super.key});
+
+  @override
+  State<LogPage> createState() => _LogPageState();
+}
+
+class _LogPageState extends State<LogPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Log Page'),
+    );
+  }
+}
---
+++ lib/pages/training_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TrainingPage extends StatefulWidget {
+  const TrainingPage({super.key});
+
+  @override
+  State<TrainingPage> createState() => _TrainingPageState();
+}
+
+class _TrainingPageState extends State<TrainingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Training Page'),
+    );
+  }
+}
