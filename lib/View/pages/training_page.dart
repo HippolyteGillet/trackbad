704eba34f5b67a569373b62eaab8d202e3bd5d17
BOULET Paul <paul.boulet.pb@gmail.com>
2024-02-16 18:38:54 +0100

    chore: fix overflow bug

--- lib/View/pages/training_page.dart
+++ lib/View/pages/training_page.dart
@@ -18,192 +18,195 @@ class _TrainingPageState extends State<TrainingPage> {
     final controller = Provider.of<Controller>(context);
     final sensors = controller.model.sensors.where((s) => s?.isActif == true).toList();
 
-    return Column(
-      children: [
-        const Padding(padding: EdgeInsets.only(top: 20)),
-        const Center(
-          child: Text(
-            'Nouvelle Séance',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontFamily: 'LeagueSpartan',
-              fontWeight: FontWeight.w900,
-              color: Color.fromRGBO(240, 54, 18, 1),
-              fontSize: 40,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Training Page'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Padding(padding: EdgeInsets.only(top: 20)),
+            const Center(
+              child: Text(
+                'Nouvelle Séance',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'LeagueSpartan',
+                  fontWeight: FontWeight.w900,
+                  color: Color.fromRGBO(240, 54, 18, 1),
+                  fontSize: 40,
+                ),
+              ),
             ),
-          ),
-        ),
-        const Padding(padding: EdgeInsets.only(top: 30)),
-        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          const Text(
-            'Ajouter un capteur',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontFamily: 'LeagueSpartan',
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-            ),
-          ),
-          const Padding(padding: EdgeInsets.only(left: 10)),
-          IconButton(
-            icon: const Icon(Icons.add_circle,
-                color: Color.fromRGBO(240, 54, 18, 1), size: 45),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const AddSensorPage()),
-              );
-            },
-          )
-        ]),
-        SizedBox(
-          width: 350,
-          height: 335,
-          child: SingleChildScrollView(
-            padding: const EdgeInsets.only(top: 25),
-            child: Wrap(
-              spacing: 5.0, // Espace horizontal entre les cartes
-              runSpacing: 30.0, // Espace vertical entre les lignes
-              children: sensors.map((sensor) {
-                return SizedBox(
-                  width: 110,
-                  height: 100,
-                  child: GestureDetector(
-                    onTap: () {
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          // Pop-up d'informations sur le capteur
-                          return AlertDialog(
-                            title: Text(sensor?.name ?? "Inconnu"),
-                            content: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Text('Joueur : ${sensor?.player?.lastname} ${sensor?.player?.firstname}'),
-                                const SizedBox(height: 8.0),
-                                Text("Batterie: ${sensor?.battery}%"),
-                                Text('Capteur : ${sensor?.uuid}'),
-                                const SizedBox(height: 8.0),
-                                Text("Type de séance : ${sensor?.seanceType.toString()}"),
-                                // Ajoutez plus d'informations ici
-                              ],
-                            ),
-                            actions: <Widget>[
-                              TextButton(
-                                child: const Text('Déconnecter',
-                                    style: TextStyle(color: Colors.red)),
-                                onPressed: () {
-                                  controller.disconnectSensor(sensor!);
-                                  Navigator.of(context)
-                                      .pop(); // Ferme la boîte de dialogue
-                                },
-                              ),
-                              TextButton(
-                                child: const Text('Fermer'),
-                                onPressed: () {
-                                  Navigator.of(context)
-                                      .pop(); // Ferme la boîte de dialogue
-                                },
-                              ),
-                            ],
+            const Padding(padding: EdgeInsets.only(top: 30)),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const Text(
+                'Ajouter un capteur',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'LeagueSpartan',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
+              ),
+              const Padding(padding: EdgeInsets.only(left: 10)),
+              IconButton(
+                icon: const Icon(Icons.add_circle,
+                    color: Color.fromRGBO(240, 54, 18, 1), size: 45),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const AddSensorPage()),
+                  );
+                },
+              )
+            ]),
+            SizedBox(
+              width: 350,
+              height: 335,
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.only(top: 25),
+                scrollDirection: Axis.horizontal,
+                child: Wrap(
+                  spacing: 5.0,
+                  runSpacing: 30.0,
+                  children: sensors.map((sensor) {
+                    return SizedBox(
+                      width: 110,
+                      height: 100,
+                      child: GestureDetector(
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Text(sensor?.name ?? "Inconnu"),
+                                content: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text('Joueur : ${sensor?.player?.lastname} ${sensor?.player?.firstname}'),
+                                    const SizedBox(height: 8.0),
+                                    Text("Batterie: ${sensor?.battery}%"),
+                                    Text('Capteur : ${sensor?.uuid}'),
+                                    const SizedBox(height: 8.0),
+                                    Text("Type de séance : ${sensor?.seanceType.toString()}"),
+                                  ],
+                                ),
+                                actions: <Widget>[
+                                  TextButton(
+                                    child: const Text('Déconnecter',
+                                        style: TextStyle(color: Colors.red)),
+                                    onPressed: () {
+                                      controller.disconnectSensor(sensor!);
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                  TextButton(
+                                    child: const Text('Fermer'),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                ],
+                              );
+                            },
                           );
                         },
-                      );
-                    },
-                    child: Card(
-                      color: const Color.fromRGBO(34, 47, 230, 1),
-                      elevation: 3,
-                      child: Stack(
-                        clipBehavior: Clip.none,
-                        alignment: Alignment.center,
-                        children: <Widget>[
-                          const Positioned(
-                            top: -25,
-                            child: CircleAvatar(
-                              radius: 25, // La taille de l'avatar
-                              backgroundColor: Colors.lightBlueAccent,
-                              child: Icon(Icons.person, size: 25),
-                            ),
-                          ),
-                          Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              const Padding(padding: EdgeInsets.only(top: 25)),
-                              FittedBox(
-                                child: Text(
-                                  sensor.player!.firstname, // Prénom
-                                  textAlign: TextAlign.center,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                  ),
+                        child: Card(
+                          color: const Color.fromRGBO(34, 47, 230, 1),
+                          elevation: 3,
+                          child: Stack(
+                            clipBehavior: Clip.none,
+                            alignment: Alignment.center,
+                            children: <Widget>[
+                              const Positioned(
+                                top: -25,
+                                child: CircleAvatar(
+                                  radius: 25,
+                                  backgroundColor: Colors.lightBlueAccent,
+                                  child: Icon(Icons.person, size: 25),
                                 ),
                               ),
-                              FittedBox(
-                                child:Text(
-                                  sensor.player!.lastname, // Nom de famille
-                                  textAlign: TextAlign.center,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
+                              Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  const Padding(padding: EdgeInsets.only(top: 25)),
+                                  FittedBox(
+                                    child: Text(
+                                      sensor.player!.firstname,
+                                      textAlign: TextAlign.center,
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
                                   ),
-                                ),
-                              ),
-
-                              const Padding(padding: EdgeInsets.only(top: 3)),
-                              FittedBox(
-                                child: Text(
-                                  '${sensor?.name}',
-                                  textAlign: TextAlign.center,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 8,
+                                  FittedBox(
+                                    child:Text(
+                                      sensor.player!.lastname,
+                                      textAlign: TextAlign.center,
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
                                   ),
-                                ),
-                              ),
-                              const Padding(padding: EdgeInsets.only(top: 10)),
+                                  const Padding(padding: EdgeInsets.only(top: 3)),
+                                  FittedBox(
+                                    child: Text(
+                                      '${sensor?.name}',
+                                      textAlign: TextAlign.center,
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 8,
+                                      ),
+                                    ),
+                                  ),
+                                  const Padding(padding: EdgeInsets.only(top: 10)),
+                                ],
+                              )
                             ],
-                          )
-                        ],
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                );
-              }).toList(),
+                    );
+                  }).toList(),
+                ),
+              ),
             ),
-          ),
-        ),
-        const Padding(padding: EdgeInsets.only(top: 15)),
-        ElevatedButton(
-          onPressed: () async {
-            await controller.startTraining();
+            const Padding(padding: EdgeInsets.only(top: 15)),
+            ElevatedButton(
+              onPressed: () async {
+                await controller.startTraining();
 
-            if (!mounted) return;
-            Navigator.push(
-                context,
-                PageRouteBuilder(
-                    pageBuilder: (_, __, ___) => const OngoingSessionPage()));
-          },
-          style: ElevatedButton.styleFrom(
-            backgroundColor: const Color.fromRGBO(240, 54, 18, 1),
-            minimumSize: const Size(250, 60),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(50.0),
-            ),
-          ),
-          child: const Text(
-            'Commencer',
-            style: TextStyle(
-              fontFamily: 'LeagueSpartan',
-              fontWeight: FontWeight.bold,
-              fontSize: 35,
-              color: Colors.white,
-            ),
-          ),
-        )
-      ],
+                if (!mounted) return;
+                Navigator.push(
+                    context,
+                    PageRouteBuilder(
+                        pageBuilder: (_, __, ___) => const OngoingSessionPage()));
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromRGBO(240, 54, 18, 1),
+                minimumSize: const Size(250, 60),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+              ),
+              child: const Text(
+                'Commencer',
+                style: TextStyle(
+                  fontFamily: 'LeagueSpartan',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 35,
+                  color: Colors.white,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
